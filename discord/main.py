9d394fc98d97969330968911a6b69cadf10c2012
NeverNotJohn <johntheherrera@gmail.com>
2023-04-24 00:00:59 -0700

    stuff

--- discord/main.py
+++ discord/main.py
@@ -1,6 +1,7 @@
 import discord  as d
 from discord.ext import commands
 import asyncio
+import datetime
 from hidden import key
 
 print('running')
@@ -9,5 +10,19 @@ print('running')
 intents = d.Intents.default()
 intents.members = True
 intents.presences = True
-client = d.Client(intents = intents)
 bot = commands.Bot(command_prefix = '!', intents = intents)
+
+@bot.event
+async def on_ready(ctx):
+    
+    print("Now we're cooking")
+
+    # Init Message
+    now = datetime.now()
+    dt_string = now.strftime(f"%d/%m/%Y %H:%M:%S")
+    await ctx.send(f"\n \n ** Run time: {dt_string} * \n \n")
+
+""" BOT KEY """
+# key located in private folder
+
+bot.run(key.botKey)
