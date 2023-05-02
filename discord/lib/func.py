3ac7101b84eaf5f7f9ce242d838d8dfbe25ff4b9
NeverNotJohn <johntheherrera@gmail.com>
2023-05-01 23:01:59 -0700

    ITS ALL COMING TOGETHER

--- discord/lib/func.py
+++ discord/lib/func.py
@@ -7,7 +7,19 @@ def draw_spaces(array):
     
     image = cv2.imread('images/parking_lot.png')
     
-    """ Rectangle vars """
+    # TOP ROW
+    for i in range(10):
+        edit_top(image, i, array[i])
+    
+    # BOTTOM ROW
+    
+    # DEBUG
+    cv2.imshow("Wow", image)
+    cv2.waitKey(0)
+    
+def edit_top(image, index, is_open):
+    
+    """ RECTANGLE VARIABLES """
     left_x = 32
     right_x = 76
     red = (255, 0, 0)
@@ -15,28 +27,12 @@ def draw_spaces(array):
     blue = (0, 0, 255)
     thickness = 2
     
-    # data[0]
+    if (is_open):
+        color = blue
+    else:
+        color = red
     
-    image = cv2.rectangle(image, (left_x, 38), (right_x, 130), red, thickness)
-    
-    # data[1]
-    # data[2]
-    # data[3]
-    # data[4]
-    # data[5]
-    # data[6]
-    # data[7]
-    # data[8]
-    # data[9]
-    # data[10]
-    # data[11]
-    # data[12]
-    # data[13]
-    
-    # DEBUG
-    cv2.imshow("Wow", image)
-    cv2.waitKey(0)
-    
+    return cv2.rectangle(image, (left_x + (47 * index), 38), (right_x + (47 * index), 130), color, thickness)
     
 
 """ DEBUG MAIN """
