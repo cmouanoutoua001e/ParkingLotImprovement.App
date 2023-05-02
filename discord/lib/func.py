3617e4290f4d959a9b663db7313829d92ea56478
NeverNotJohn <johntheherrera@gmail.com>
2023-05-01 23:14:39 -0700

    kinda done now

--- discord/lib/func.py
+++ discord/lib/func.py
@@ -12,7 +12,10 @@ def draw_spaces(array):
         edit_top(image, i, array[i])
     
     # BOTTOM ROW
-    
+    for i in range(7):    
+        j = i + 10          # array offset (to align with x_offset)
+        edit_bot(image, i, array[j])
+        
     # DEBUG
     cv2.imshow("Wow", image)
     cv2.waitKey(0)
@@ -21,7 +24,7 @@ def edit_top(image, index, is_open):
     
     """ RECTANGLE VARIABLES """
     left_x = 32
-    right_x = 76
+    right_x = 73
     red = (255, 0, 0)
     yellow = (255, 255, 0)
     blue = (0, 0, 255)
@@ -34,12 +37,28 @@ def edit_top(image, index, is_open):
     
     return cv2.rectangle(image, (left_x + (47 * index), 38), (right_x + (47 * index), 130), color, thickness)
     
+def edit_bot(image, index, is_open):
+    
+    """ RECTANGLE VARIABLES """
+    left_x = 170
+    right_x = 211
+    red = (255, 0, 0)
+    yellow = (255, 255, 0)
+    blue = (0, 0, 255)
+    thickness = 2
+    
+    if (is_open):
+        color = blue
+    else:
+        color = red
+    
+    return cv2.rectangle(image, (left_x + (47 * index), 410), (right_x + (47 * index), 502), color, thickness)
 
 """ DEBUG MAIN """
 
 def main():
     
-    data = [True, False, True, False, True, False, True, False, True, False, True, False, True, False]
+    data = [True, False, False, False, False, True, True, True, True, True, False, False, False, True, True, True, True]
     #df = pd.Dataframe(data, columns="availability")
 
     print("Running")
