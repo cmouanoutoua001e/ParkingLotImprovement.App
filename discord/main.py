ef9d29c468fdeecdbd79ff5b52f9808f9001ce8a
NeverNotJohn <johntheherrera@gmail.com>
2023-05-02 21:18:48 -0700

    auto sending message and stuff

--- discord/main.py
+++ discord/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext import tasks
 import datetime
 from hidden import key
 from lib import func
@@ -17,6 +18,15 @@ bot = commands.Bot(command_prefix = '!', intents = intents)
 """ Global vars """
 
 data = [False, False, False, False, True, True, True, False, False, False, True, True, True, True, False, True, True] # FIXME
+bot_channel = 1099960596943863818
+general_channel = 1099948525892149280
+park_channel = 1103156024967434350
+
+"""
+
+    BOT EVENTS
+
+"""
 
 @bot.event
 async def on_ready():
@@ -24,15 +34,23 @@ async def on_ready():
     print("Now we're cooking")
     
     # channels:
-    general = bot.get_channel(1099948525892149280) #FIXME
-    beep = bot.get_channel(1099960596943863818)
+    general = bot.get_channel(general_channel) #FIXME
+    beep = bot.get_channel(bot_channel)
 
-    # Init Message
+    # Init Message DEBUG
     now = datetime.datetime.now()
     dt_string = now.strftime(f"%d/%m/%Y %H:%M:%S")
     await beep.send(f"\n \n ** Run time: {dt_string} * \n \n")
     
-""" UPDATE COMMAND """
+    # START LOOPS
+    
+    await mainLoop.start()
+    
+"""
+
+    COMMANDS!!!
+
+"""
 # 1. requests openCV data
 # 2. Changes parking lot map to match open sports
 # 3. send to discord channel
@@ -45,8 +63,18 @@ async def update(ctx):
     with open('space.png', 'rb') as f:
         picture = discord.File(f)
         await ctx.send("Engineering East Parking Lot: ", file=picture)
-        await ctx.send("Blue = OPEN \nRed = CLOSED")
+        await ctx.send("Blue = OPEN                 Red = CLOSED")
         
+    total_closed = 0
+    for i in data:      # count total open
+        if (not i):
+            total_closed = total_closed + 1
+    
+    percentage_closed = int ( ( (total_closed) / (len(data)) ) * 100 )
+    
+    await ctx.send(f"Engineering West Parking Lot is {percentage_closed}% full", tts=True)
+        
+"""
 @bot.command()
 async def talk(ctx):
     
@@ -58,6 +86,42 @@ async def talk(ctx):
     percentage_closed = int ( ( (total_closed) / (len(data)) ) * 100 )
     
     await ctx.send(f"Engineering West Parking Lot is {percentage_closed}% full", tts=True)
+"""
+
+"""
+
+    PROCEDURALLY RUN TASKS
+
+"""
+
+@tasks.loop(seconds=60)
+async def mainLoop():
+    
+    print("LOOPED")     #debug stuff
+    
+    park = bot.get_channel(park_channel)
+    
+    # Send image
+    
+    func.draw_spaces(data, 'lib/images/parking_lot.png') # REPLACE DATA WITH NICK'S OUTPUT
+    
+    # Delete previous messages
+    await park.purge(limit=10)
+    
+    with open('space.png', 'rb') as f:
+        picture = discord.File(f)
+        await park.send("Engineering East Parking Lot: ", file=picture)
+        await park.send("Blue = OPEN                 Red = CLOSED")
+        
+    total_closed = 0
+    for i in data:      # count total open
+        if (not i):
+            total_closed = total_closed + 1
+    
+    percentage_closed = int ( ( (total_closed) / (len(data)) ) * 100 )
+    
+    
+    await park.send(f"Engineering West Parking Lot is {percentage_closed}% full", tts=True)
 
 """ BOT KEY """
 # key located in private folder
