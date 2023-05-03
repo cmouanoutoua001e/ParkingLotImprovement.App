1c3c7546bb594bb0240aa19a78678bcd58ef3b1e
cmouanoutoua001e <cmouanoutoua001@mail.fresnostate.edu>
2023-05-02 23:33:14 -0700

    it works

---
+++ dotNETMaui.ProjectFiles/Service/ReadAloud.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Media;
+using System.Speech.Synthesis;
+
+namespace ParkingLotImprovement.App.Service
+{
+    public class ReadAloudModel
+    {
+        // Public Variables
+        public string ToRead { get; set; }
+
+        // Public Methods
+        public async void ReadAloud()
+        {
+            await TextToSpeech.Default.SpeakAsync(ToRead, options);
+        }
+        public void Set(string[] labels)
+        {
+            ToRead =
+                labels[0] + " is " + labels[1] + ".  There " +          // 'P21' is '47% Full'.  There 
+                ((labels[3] == "1") ? ("is" + labels[3] + "stall")      // (is '1' stall)
+                : ("are" + labels[3] + "stalls")) + "open.";             // (are '9' stalls) open.
+        }
+
+
+        // Private Variables
+        SpeechOptions options = new()
+        {
+            Pitch = 2f,
+            Volume = 1f
+        };
+
+        // Constructors
+        public ReadAloudModel() { }
+    }
+}
