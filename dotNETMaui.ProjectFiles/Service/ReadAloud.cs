c6729cd1f92d634054763d393df431246642e10a
cmouanoutoua001e <cmouanoutoua001@mail.fresnostate.edu>
2023-06-10 11:34:45 -0700

    Need to fix android speech stuff

--- dotNETMaui.ProjectFiles/Service/ReadAloud.cs
+++ dotNETMaui.ProjectFiles/Service/ReadAloud.cs
@@ -21,9 +21,11 @@ namespace ParkingLotImprovement.App.Service
             }
 
             ToRead =
-                labels[0] + " is " + labels[1] + ".  There " +          // 'P21' is '47% Full'.  There 
-                ((labels[3] == "1") ? ("is" + labels[3] + "stall")      // (is '1' stall)
-                : ("are" + labels[3] + "stalls")) + "open.";             // (are '9' stalls) open.
+                labels[0] + " is " + labels[1] + ".  There " +      // 'P21' is '47% Full'.  There 
+                ((labels[3] == "1") ?
+                ("is" + labels[3] + "stall") :                      // (is '1' stall)
+                ("are" + labels[3] + "stalls")) +                   // (are '9' stalls)
+                "open.";                                            // open.
         }
 
 
